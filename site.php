82bd97b0b43f59307320dd5f9b8790003641becd
Job V <soporte@redchapina.com>
2022-03-31 08:42:14 -0600

    home 28-03-22

--- site.php
+++ site.php
@@ -8,7 +8,7 @@
 
 <body>
 
-     <!-- <form action="site.php" method="session">
+    <!-- <form action="site.php" method="session">
         Name: <input type="text" name="name">
         Age: <input type="text" name="age"> <br><br>
         Password: <input type="password" name="password"> <br><br>
@@ -27,19 +27,26 @@
     // $_SESSION    // Great for collect data that is sensitive info. Session will be destroyed if the website is closed.
     // $FILES
 
-    session_start();
+    // session_start();
 
-    //storing information
+    // //storing information
 
-    $_SESSION ["Name"] = "job";
-    $_SESSION ["Age"] = 32;
+    // $_SESSION ["Name"] = "job";
+    // $_SESSION ["Age"] = 32;
 
-    echo "Hello " . $_SESSION["Name"] . "<br>";
+    // echo "Hello " . $_SESSION["Name"] . "<br>";
 
-    echo "test";
+    // echo "test"
 
+    $targetDays = mktime(0, 0, 0, 11, 11, 2022); // param1: Hours, param2: minutes, param3: seconds param4,5,6: Day, Month, Current year; Number 4,5,6 are the param that we want to add
 
+    $today = time(); //Create a variable with the current time
 
+    $differenceDays = ($targetDays - $today); //Creating a new variable with the operation.
+
+    $days = (int)($differenceDays / 86400);
+    echo "Faltan " . $days . " días para el cumpleaños de Jhoany " . "<br>";
+    
     ?>
 
 </body>
