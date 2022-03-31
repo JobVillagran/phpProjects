b93fde96d0751758e5c98634ffde6a1de8436138
Job V <soporte@redchapina.com>
2022-03-31 08:41:54 -0600

    home 28-03-22

--- exercises_two/birthday_countdown.php
+++ exercises_two/birthday_countdown.php
@@ -13,14 +13,14 @@
     // Create a birthday countdown script
     // that counts the days between the current day and your Bday
     
-    $targetDays = mktime(0, 0, 0, 9, 9, 2022); // param1: Hours, param2: minutes, param3: seconds param4,5,6: Day, Month, Current year; Number 4,5,6 are the param that we want to add
+    $targetDays = mktime(0, 0, 0, 11, 11, 2022); // param1: Hours, param2: minutes, param3: seconds param4,5,6: Day, Month, Current year; Number 4,5,6 are the param that we want to add
 
     $today = time(); //Create a variable with the current time
 
     $differenceDays = ($targetDays - $today); //Creating a new variable with the operation.
 
     $days = (int)($differenceDays / 86400);
-    echo "Days until next birthday: " . $days . " days!" . "<br>";
+    echo "Faltan " . $days . " para el cumpleaños de Jhoany " . "<br>";
 
 
     ?>
